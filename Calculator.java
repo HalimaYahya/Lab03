5e77ebf102e0b838147133c12d21c4e2e6904417
Halima <201720504@ammanu.edu.jo>
2021-11-08 16:22:48 +0400

    add    new file:   new file:   Calculator.java and  testCalculator.java

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+public class Calculator
+{
+    private ArrayList myList;
+
+    public double Average (ArrayList values)
+    { 
+  
+      ArrayList myList = new ArrayList<Double>(2.3, 5, 11 ,17, 100,0.7 );
+          for (double i : myList) {
+           System.out.println("The values: "+i);
+           }
+    }
+     public void setValues(double values)
+    {  
+         int values = 0;
+          int avg;
+          for(int i = 0; i < myList.size(); i++)
+          {
+            values += myList.get(i);
+            avg = values / myList.size();
+            System.out.println("The Average is: " + avg);
+         }
+   }
+   
+   
+}
---
+++ testCalculator.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class testCalculator
+{
+   public static void main(String [] args)
+   {
+    Calculator cal = new Calculator();
+     cal.Average();
+     cal.setValues();
+     
+   }
+}
